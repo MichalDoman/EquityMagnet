616517f5cb832c65e9a70e591bc5da0fd9409a32
michaldoman <mjd.domanski@gmail.com>
2023-06-22 12:08:28 +0200

    .gitignore

---
+++ equity_magnet/equity_magnet/local_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default': {
+        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'final_project_db',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+    }
+}
+
+API_KEY = "566b84b1d223b010c6be47cf0bc0bce2"
+# API_KEY = "429da98c445a02548c83cb5eb73ce2f1"
+# API_KEY = "6a5c4821aa277d716732ea2cd74fde79"
+# API_KEY = "fd176e483095e94a648a283c593dea0f"
